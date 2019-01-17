a5c20a4d698adf15f0298a98226a53c98b22f268
Thomas Yuba <yubathom@gmail.com>
2019-01-17 14:06:35 -0200

    Setup instalacao local (#1)
    * Pequenas alteracoes nos endopoints sem parametros, altera paramtro padrao de 'data' para 'updates' e configuracao para apache (.htacess)
    
    * Pequena alteracao do README.md

--- classes/getter.php
+++ classes/getter.php
@@ -1,24 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-type: application/json");
-
-class Getter{
+class Getter {
 	public function getRoute($d){
-		//$out = new stdClass();
-		// echo $GLOBALS['data'];
-				$sql = "SELECT post_id, meta_value FROM wp_postmeta WHERE meta_key LIKE '_wp_attached_file' LIMIT ".$d;  
+		$sql = "SELECT post_id, meta_value FROM wp_postmeta WHERE meta_key LIKE '_wp_attached_file' LIMIT " . $d;  
+		$result = mysqli_query($GLOBALS['link'], $sql);  
+		$json_array = array();  
 
-				$result = mysqli_query($GLOBALS['link'], $sql);  
-				$json_array = array();  
-
-				while($row = mysqli_fetch_assoc($result))  
-				{
-					$json_array[] = $row;  
-				} 
-
-				echo json_encode($json_array);
-		//return json_encode($out);
+		while($row = mysqli_fetch_assoc($result))  
+		{
+			$json_array[] = $row;  
+		} 
+		return json_encode($json_array);
 	}
 }
-
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 <?php
-// conexão com o banco de dados
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json");
+
+// varávies de ambiente
 require_once 'conexao.env';
 
-if(isset($_GET["data"])){
+// conectar com o banco de dados
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// checar conexão
+if($link === false){
+    die("ERRO: Não foi possível conectar. " . mysqli_connect_error());
+}
+
+if(isset($_GET["updates"])){
 	include 'classes/getter.php';
-	$data = $_GET["data"];
-	$response = new Getter($data);
-	echo $response->getRoute($data);
+	$updates = $_GET["updates"];
+	$response = new Getter($updates);
+	echo $response -> getRoute($updates);
 }
 else {
 	$endpoints = array(
-		"listar últimas atualizações das mídias do wordpress (apenas 1, 2 e 3)" => '/?data=wp-midias/:id'
+		"liste últimas atualizações das mídias do wordpress" => '/?updates=:Number',
+		"Exemplo. Retorna últimas três mídias" => $_SERVER['SCRIPT_URI'] . '?updates=3',
 	);
 	echo json_encode($endpoints); 
 }
-
-
-?>  
+?>
